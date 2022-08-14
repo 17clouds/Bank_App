905831b434fee823e778f1ba1954f81535bf00ff
17clouds <prosvirnin0121@gmail.com>
2022-08-14 16:12:45 +0300

    completed the registration section

--- Bank/app.js
+++ Bank/app.js
@@ -35,9 +35,29 @@ function onLinkClick(event) {
 window.onpopstate = () => updateRoute();
 updateRoute();
 
-function register() {
+async function register() {
     const registerForm = document.getElementById('registerForm');
     const formData = new FormData(registerForm);
     const data = Object.fromEntries(formData);
     const jsonData = JSON.stringify(data);
+    const result = await createAccount(jsonData);
+
+    if (result.error) {
+        return console.log('An error occurred:', result.error);
+    }
+
+    console.log('Account created!', result);
+}
+
+async function createAccount (account) {
+    try {
+        const response = await fetch('//localhost:5000/api/accounts', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: account
+        });
+        return await response.json();
+    } catch (error) {
+        return {error: error.message || 'Unknown error'};
+    }
 }
